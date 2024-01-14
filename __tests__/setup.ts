fb5028ee0f9b2f5a407d4bd564a13006fec54e84
xyashino <szczypiorpan123@gmail.com>
2024-01-14 20:01:18 +0100

    refactor(prettier): run prettify command

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,14 +1,11 @@
 import { afterEach, expect } from 'vitest';
 
 import * as matchers from '@testing-library/jest-dom/matchers';
-import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
-
+import { cleanup } from '@testing-library/react';
 
 expect.extend(matchers);
 
 afterEach(() => {
-    cleanup();
+  cleanup();
 });
-
-
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [tsconfigPaths(), react()],
-    test: {
-        environment: "jsdom",
-        setupFiles: "./__tests__/setup.ts",
-        globals: true,
-    },
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './__tests__/setup.ts',
+    globals: true,
+  },
 });
